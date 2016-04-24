3014a87b2460a590246d25785c4427268b674783
Frank La Vigne <frankchevy@hotmail.com>
2016-04-23 22:33:26 -0400

    Setting up permissions and code structure

--- WiFIScannerUWP/App.xaml.cs
+++ WiFIScannerUWP/App.xaml.cs
@@ -43,7 +43,7 @@ namespace WiFIScannerUWP
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                this.DebugSettings.EnableFrameRateCounter = false;
             }
 #endif
 
--- WiFIScannerUWP/MainPage.xaml.cs
+++ WiFIScannerUWP/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Devices.WiFi;
+using System.Collections.ObjectModel;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +25,40 @@ namespace WiFIScannerUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        private WiFiAdapter _firstWiFiAdapter;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            InitializeScanner();
+
+        }
+
+        private async void InitializeScanner()
+        {
+            var access = await WiFiAdapter.RequestAccessAsync();
+
+            if (access != WiFiAccessStatus.Allowed)
+            {
+                // permission error
+            }
+            else
+            {
+                var wifiAdapterResults = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(WiFiAdapter.GetDeviceSelector());
+
+                if (wifiAdapterResults.Count >= 1)
+                {
+                    this._firstWiFiAdapter = await WiFiAdapter.FromIdAsync(wifiAdapterResults[0].Id);
+                }
+                else
+                {
+                    //error
+                }
+            }
+
+
         }
     }
 }
